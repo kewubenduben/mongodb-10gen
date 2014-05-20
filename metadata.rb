da3cd0c9e3de53a57dc940504086402548afc54b
Ker Ruben Ramos <xdiscent@gmail.com>
2014-05-20 10:20:51 +0800

    Don't depend itself
    Depending on itself breaks dependency resolvers

--- metadata.rb
+++ metadata.rb
@@ -6,6 +6,5 @@ description      "Installs/Configures mongodb-10gen"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.1.10"
 depends          "apt"
-depends          "mongodb-10gen" # workaround for TravisCI
 supports         "ubuntu"
 supports         "debian"
